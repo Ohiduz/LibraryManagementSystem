9c631e957937d21d457c773eb66b6f8dffa6a337
Ohiduz <ohiduzzaman1618@gmail.com>
2022-11-20 21:05:46 +0530

    creation of tables at the beginning

--- src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
+++ src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
@@ -15,6 +15,70 @@ import java.sql.PreparedStatement;
 public class LibraryManagementSystem {
 
     public static void main(String[] args) {
+        Connection con;
+        try{
+            con = DBConnection.getConnection();
+            String del = "drop table students;";
+            PreparedStatement ds = con.prepareStatement(del);
+            int c = ds.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println("");
+        }
+        try{
+            con = DBConnection.getConnection();
+            String del = "drop table books;";
+            PreparedStatement ds = con.prepareStatement(del);
+            int c = ds.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println("");
+        }
+        try{
+            con = DBConnection.getConnection();
+            String del = "drop table issueBooks;"; 
+            PreparedStatement ds = con.prepareStatement(del);
+            int c = ds.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println("");
+        }
+        try{
+            con = DBConnection.getConnection();
+            String sq = "create table students (studentId varchar(20) not null primary key, "
+                    + "studentName varchar(50) not null, "
+                    + "email varchar(50) not null, "
+                    + "password varchar(50) not null, booksBorrowed int default 0);";
+            PreparedStatement pst = con.prepareStatement(sq);
+            int tablescount = pst.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            con = DBConnection.getConnection();
+            String sq = "create table books (bookId varchar(10) not null primary key, "
+                    + "bookName varchar(50) not null, "
+                    + "quantity int);";
+            PreparedStatement pst = con.prepareStatement(sq);
+            int tablescount = pst.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            con = DBConnection.getConnection();
+            String sq = "create table issueBooks (issueId int not null auto_increment primary key, "
+                    + "studentId varchar(10) not null, "
+                    + "bookId varchar(10) not null, "
+                    + "from Date, "
+                    + "to Date, Status varchar(10);";
+            PreparedStatement pst = con.prepareStatement(sq);
+            int tablescount = pst.executeUpdate();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
         StartPage sp = new StartPage();
         sp.setVisible(true);
     }
